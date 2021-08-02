6ff3b3249cf63ed9b656617e049212949787860c
Elvira <57684716+Elvira654@users.noreply.github.com>
2021-08-02 14:24:00 +0200

    Add files via upload

---
+++ Zadatak 2/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIme = new System.Windows.Forms.Label();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.lblPrezime = new System.Windows.Forms.Label();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.cmdSnimi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblIme
+            // 
+            this.lblIme.AutoSize = true;
+            this.lblIme.Location = new System.Drawing.Point(29, 48);
+            this.lblIme.Name = "lblIme";
+            this.lblIme.Size = new System.Drawing.Size(24, 13);
+            this.lblIme.TabIndex = 0;
+            this.lblIme.Text = "&Ime";
+            this.lblIme.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(78, 48);
+            this.txtIme.MaxLength = 15;
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(100, 20);
+            this.txtIme.TabIndex = 1;
+            this.txtIme.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblPrezime
+            // 
+            this.lblPrezime.AutoSize = true;
+            this.lblPrezime.Location = new System.Drawing.Point(17, 120);
+            this.lblPrezime.Name = "lblPrezime";
+            this.lblPrezime.Size = new System.Drawing.Size(44, 13);
+            this.lblPrezime.TabIndex = 2;
+            this.lblPrezime.Text = "&Prezime";
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(78, 120);
+            this.txtPrezime.MaxLength = 25;
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(100, 20);
+            this.txtPrezime.TabIndex = 3;
+            // 
+            // cmdSnimi
+            // 
+            this.cmdSnimi.Location = new System.Drawing.Point(174, 183);
+            this.cmdSnimi.Name = "cmdSnimi";
+            this.cmdSnimi.Size = new System.Drawing.Size(75, 23);
+            this.cmdSnimi.TabIndex = 4;
+            this.cmdSnimi.Text = "&Snimi";
+            this.cmdSnimi.UseVisualStyleBackColor = true;
+            this.cmdSnimi.Click += new System.EventHandler(this.cmdSnimi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.cmdSnimi);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.lblPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.lblIme);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIme;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.Label lblPrezime;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.Button cmdSnimi;
+    }
+}
+
